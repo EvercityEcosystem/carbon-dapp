b0354868f34a1176c6d78d229181c4217b850b9e
Ilya Ozerov <arnage92@gmail.com>
2022-08-03 19:52:28 +0300

    feature: address encoding by network

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import keyring from "@polkadot/ui-keyring";
+import { BrowserRouter } from "react-router-dom";
+import { cryptoWaitReady } from "@polkadot/util-crypto";
+
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
 import { PolkadotProvider } from "./components/PolkadotProvider";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <PolkadotProvider>
-        <App />
-      </PolkadotProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+cryptoWaitReady().then(() => {
+  keyring.loadAll({ ss58Format: import.meta.env.VITE_NETWORK_CODE });
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <PolkadotProvider>
+          <App />
+        </PolkadotProvider>
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+});
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,6 +1,22 @@
-const getCurrentUser = () => ({
-  address: localStorage.getItem("userAddress"),
-});
+import keyring from "@polkadot/ui-keyring";
+
+const getCurrentUser = () => {
+  const rawAddress = localStorage.getItem("userAddress");
+
+  if (!rawAddress) {
+    return {};
+  }
+
+  const decodedAdress = keyring.decodeAddress(rawAddress);
+  const encodedAddress = keyring.encodeAddress(
+    decodedAdress,
+    import.meta.env.VITE_NETWORK_CODE,
+  );
+
+  return {
+    address: encodedAddress,
+  };
+};
 
 const saveCurrentUser = ({ address }) => {
   if (!address) {
