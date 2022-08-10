c559975db9c4bb65d47201c32775bec2243038af
Ilya Ozerov <arnage92@gmail.com>
2022-08-10 11:29:41 +0300

    rm useless

--- src/utils/polkadot.js
+++ src/utils/polkadot.js
@@ -1,22 +1,19 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { web3FromSource, web3Enable } from "@polkadot/extension-dapp";
 
-import types from "../../types.json";
-
 const wsProvider = new WsProvider(import.meta.env.VITE_WS_PROVIDER_URL);
 
 wsProvider.on("disconnected", () => {
   console.warn("Provider disconnected. Reconnecting...");
 });
 
-wsProvider.on("error", error => {
+wsProvider.on("error", (error) => {
   console.error("Provider error: ", error);
 });
 
 const connect = async () =>
   ApiPromise.create({
     provider: wsProvider,
-    types,
   });
 
 const getInjector = async () => {
